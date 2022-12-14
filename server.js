c24d17ac7e62b5fd0279da830e07c56c53042d1a
dylan <dylan@kses.net>
2022-12-14 18:53:15 +0200

    fuck

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const nStatic = require('node-static');
-const fileServer = new nStatic.Server('./dist');
+const fileServer = new nStatic.Server('./public_html');
 http.createServer(function (req, res) {
   fileServer.serve(req, res);
 }).listen(3000);
